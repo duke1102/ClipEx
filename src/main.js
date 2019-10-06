62cdb3a5fce59e24e3b271e9003bbbd4a035845f
Danny <duke1102@users.noreply.github.com>
2019-10-06 16:16:12 +0200

    Updated to twitch 3.x

--- src/main.js
+++ src/main.js
@@ -13,6 +13,8 @@ yargs
 
 dotenv.config();
 
+const CLIP_TYPE = yargs.argv.game;
+
 if (
   (yargs.argv.token == undefined || typeof yargs.argv.token == "boolean") &&
   (yargs.argv.clientid == undefined ||
@@ -35,12 +37,18 @@ if (
 }
 const startAt = isNaN(yargs.argv.start) ? 0 : Math.abs(yargs.argv.start);
 const clientId = yargs.argv.clientid || process.env.TWITCH_CLIENT_ID;
-const accessToken = yargs.argv.token || process.env.TWITCH_ACCESS_TOKEN;
+const clientSecret =
+  yargs.argv.clientsecret || process.env.TWITCH_CLIENT_SECRET;
+const refreshToken =
+  yargs.argv.refreshtoken || process.env.TWITCH_REFRESH_TOKEN;
+const accessToken = yargs.argv.accesstoken || process.env.TWITCH_ACCESS_TOKEN;
+const limit = yargs.argv.limit || 100;
+const startDate = yargs.argv.start || undefined;
+const endDate = yargs.argv.end || undefined;
 
 const downloadFile = async (url, fileName) => {
   const filePath = `./clips_${yargs.argv.user}/${fileName}`;
   const writer = fs.createWriteStream(filePath);
-
   const response = await axios({
     url,
     method: "GET",
@@ -59,36 +67,63 @@ const clipex = async () => {
   await fs.mkdir(`./clips_${yargs.argv.user}`, err => {
     return err;
   });
+
   const twitchClient = await TwitchClient.withCredentials(
     clientId,
-    accessToken
+    accessToken,
+    undefined,
+    {
+      clientSecret,
+      refreshToken,
+      onRefresh: token => {
+        console.log("refresh token", token);
+      }
+    }
   );
-  const user = await twitchClient.helix.users.getUserByName(yargs.argv.user);
+  const user = !CLIP_TYPE
+    ? await twitchClient.helix.users.getUserByName(yargs.argv.user)
+    : await twitchClient.helix.games.getGameByName(yargs.argv.user);
   if (user == null) {
-    console.error(`Error, couldn't find the user "${yargs.argv.user}" on Twitch.`);
+    console.error(
+      `Error, couldn't find the user "${yargs.argv.user}" on Twitch.`
+    );
     process.exit();
   }
-  const request = await twitchClient.helix.clips.getClipsForBroadcaster(
-    user.id
-  );
-  let clipser = await request.getAll();
-
-  console.log(`Found ${clipser.length} clips.`);
-  if (startAt > clipser.length) {
+  const request = !CLIP_TYPE
+    ? await twitchClient.helix.clips.getClipsForBroadcaster(user.id, {
+        limit: limit,
+        startDate: startDate,
+        endDate: endDate
+      })
+    : await twitchClient.helix.clips.getClipsForGame(user.id, {
+        limit: limit,
+        startDate: startDate,
+        endDate: endDate
+      });
+  let clipser = await request;
+  console.log(`Found ${clipser.data.length} clips.`);
+  if (startAt > clipser.data.length) {
     console.error("Given start index greater than clips found, exiting.");
     process.exit();
   }
 
-  for (let i = startAt; i < clipser.length; i++) {
-    const element = clipser[i];
+  for (let i = startAt; i < clipser.data.length; i++) {
+    const element = clipser.data[i];
     let tmpUrl = element._data.thumbnail_url;
     tmpUrl = tmpUrl.replace(/-preview-.{1,}\.{1}.{1,}$/gim, ".mp4");
     let tmpName = element._data.id + ".mp4";
-    console.log(`Downloading ${i + 1}/${clipser.length} -> ${tmpName}`);
-    await downloadFile(tmpUrl, `${user.id}_${tmpName}`).catch(err => {
+    const identifier = CLIP_TYPE
+      ? element._data.broadcaster_name
+      : yargs.argv.user;
+    console.log(
+      `Downloading ${i + 1}/${clipser.data.length} -> ${identifier}_${tmpName}`
+    );
+    await downloadFile(tmpUrl, `${identifier}_${tmpName}`).catch(err => {
       console.error(`Error downloading "${tmpName}"`);
     });
   }
 };
 
-clipex();
+clipex().catch(err => {
+  console.error(err);
+});
